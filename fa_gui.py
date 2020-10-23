84e2d8686bd2e7c932bb4a80fcfee7352517e9d6
Lasse Meyer <meyer.lasse@gmail.com>
2020-10-23 18:32:38 +0200

    Use UI files in fa_gui.py and set appropriate fields when adding a new stock

--- fa_gui.py
+++ fa_gui.py
@@ -1,4 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QSizePolicy
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5.QtGui import QIcon
+
+import fa_backend as fab
+
+UI_FILE_MAINWINDOW = "fa_mainwindow.ui"
+UI_FILE_STOCK = "stock.ui"
 
 WINDOW_TITLE = "Fundamental Analysis Tool"
 
@@ -6,20 +13,49 @@ class FaMainWindow(QMainWindow):
 
     def __init__(self, *args):
         QMainWindow.__init__(self, *args)
+        uic.loadUi(UI_FILE_MAINWINDOW, self)
         self.setWindowTitle(WINDOW_TITLE)
-        self.central_widget = QWidget()
-        self.central_layout = QVBoxLayout()
-        self.central_widget.setLayout(self.central_layout)
-        self.setCentralWidget(self.central_widget)
-        self.label_test = QLabel("Test-Label")
-        self.central_layout.addWidget(self.label_test)
-        self.label_test1 = QLabel("Test-Label")
-        self.central_layout.addWidget(self.label_test1)
-        self.statusBar().showMessage("Ready")
-        #self.setFixedSize(self.central_layout.sizeHint())
-        self.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,
-                                       QSizePolicy.Fixed))
+        self.setWindowIcon(QIcon("icon.png"))
+        self.input_add_stock.returnPressed.connect(self.handle_add_stock)
+        self.button_add_stock.clicked.connect(self.handle_add_stock)
+        self.button_clear_stock.clicked.connect(self.handle_clear_stock)
+        self.count_stock_layouts = 0
 
     def set_version_info(self, version):
         combined = "{} v{}".format(WINDOW_TITLE, version)
         self.setWindowTitle(combined)
+
+    def handle_add_stock(self):
+        try:
+            new_ticker = fab.get_ticker(self.input_add_stock.text())
+            self.add_stock(new_ticker)
+            self.handle_clear_stock()
+        except fab.TickerNotFoundException:
+            print("invalid ticker name")
+
+    def handle_clear_stock(self):
+        self.input_add_stock.clear()
+
+    def add_stock(self, ticker):
+        stock = StockWidget(ticker)
+        self.list_stocks.insertWidget(self.count_stock_layouts, stock)
+        self.count_stock_layouts += 1
+
+class StockWidget(QWidget):
+
+    def __init__(self, ticker):
+        QWidget.__init__(self)
+        uic.loadUi(UI_FILE_STOCK, self)
+
+        name = "{} ({})".format(fab.get_name(ticker), fab.get_symbol(ticker))
+        currency = fab.get_currency(ticker)
+        ask = fab.get_formatted_currencystring(fab.get_ask(ticker), currency)
+        askbid_spread = fab.get_formatted_currencystring(fab.get_askbid_spread(ticker), currency)
+        eps_trailing = fab.get_formatted_currencystring(fab.get_eps_trailing(ticker), currency)
+        eps_forward = fab.get_formatted_currencystring(fab.get_eps_forward(ticker), currency)
+
+        self.label_name.setText(name)
+        self.label_ask.setText(ask)
+        self.label_askbid_spread.setText(askbid_spread)
+        self.label_eps_trailing.setText(eps_trailing)
+        self.label_eps_forward.setText(eps_forward)
