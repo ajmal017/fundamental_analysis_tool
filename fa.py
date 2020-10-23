86ffb6b8376df5d9d0fe0ba305df1c3182cf692a
Lasse Meyer <meyer.lasse@gmail.com>
2020-10-23 18:30:53 +0200

    Set icon as windows taskbar icon

--- fa.py
+++ fa.py
@@ -1,15 +1,17 @@
 import sys
+import ctypes
 
 from PyQt5.QtWidgets import QApplication
 
-import fa_backend as fab
 from fa_gui import FaMainWindow
 
+WINAPPID = "meyerlasse.fundamental_analysis_tool"
 VERSION = "0.0.1"
 
 if __name__ == "__main__":
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(WINAPPID)
     app = QApplication(sys.argv)
     main_window = FaMainWindow()
     main_window.set_version_info(VERSION)
     main_window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
