25384fa621ed0beb1aa1fb11cf0b88e32c2fb37e
Lasse Meyer <meyer.lasse@gmail.com>
2020-10-23 19:54:00 +0200

    Add platform specific check in main function

--- fa.py
+++ fa.py
@@ -11,7 +11,8 @@ WINAPPID = "meyerlasse.fundamental_analysis_tool"
 VERSION = "0.0.1"
 
 if __name__ == "__main__":
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(WINAPPID)
+    if sys.platform == "win32":
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(WINAPPID)
     app = QApplication(sys.argv)
     main_window = FaMainWindow()
     main_window.set_version_info(VERSION)
